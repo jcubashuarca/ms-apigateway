248085ae795002309acd3733ee5f8787848000f5
Jose Daniel Cubas Huarca <jcubashu@emeal.nttdata.com>
2023-09-20 14:10:28 -0500

    Subida de MS apigateway

---
+++ src/main/java/ccom/nttdata/bootcamp/msapitgateway/MsApitgatewayApplication.java
@@ -0,0 +1,13 @@
+package ccom.nttdata.bootcamp.msapitgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsApitgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsApitgatewayApplication.class, args);
+	}
+
+}
